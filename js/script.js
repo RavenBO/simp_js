395740ceeedee781a9d0ac69418827fd6faa3990
Stanislav <irshenko070@gmail.com>
2022-03-22 19:04:08 +0200

    functions

--- js/script.js
+++ js/script.js
@@ -22,30 +22,63 @@
 Проверить, чтобы все работало без ошибок в консоли */
 
 'use strict';
-
-let numberOfFilms=+prompt('How much films do you watched ?','15');
-let a =[];    
+let numberOfFilms; 
+function showMyDB(stat){
+    if(!stat){
+        console.log(personalMovieDB);
+    }
+}
+function start (){
+    numberOfFilms= +prompt('How much films do you watched ?','15');
+    while(numberOfFilms ==""|| numberOfFilms==null ||isNaN(numberOfFilms)){
+        numberOfFilms= +prompt('How much films do you watched ?',''); 
+    }
+}
+function writeYourGenres (genres) {
+    for(let i=0; i<3; i++){
+        genres[i]=prompt(`Ваш любимый жанр под номером ${i+1}`,'');
+    }
+    
+}
+   
 const personalMovieDB ={
     count: numberOfFilms,
     movies: {},
     actors: {},
     gennres: [],
-    privat : true,
+    privat : false,
 
 };
-console.log(personalMovieDB.count);
-let i=0;
-while(i<4){
-    a[i]= prompt("Один із останніх фільміф які ви переглядали", '');
-    i++;
-    a[i] = +prompt("На скільки оціните його по 10-ти більній шкалі", ''); 
-    i++;
+
+const cnt = personalMovieDB.count;
+
+function rememberMyFilms(){
+    for(let i =0; i<2; i++){
+        const a=prompt("Один із останніх фільміф які ви переглядали", ''),
+              b=prompt("На скільки оціните його по 10-ти більній шкалі", '');
+        if(a!=null && b!=null && a.length >0 && a.length <51 && b.length > 0 ){
+            personalMovieDB.movies[a]=b;
+        } else {
+            i--;
+        }
+        
+    }
 }
-/* console.log(personalMovieDB.movies.film);
-console.log(personalMovieDB.movies.rate); */
-let j=0;
 
-personalMovieDB.movies[a[0]]=a[1];
-personalMovieDB.movies[a[2]]=a[3];
+function MyFilmRate() {
+    if(cnt < 10 && cnt!=0){
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (cnt > 10 && cnt < 30 ){
+        console.log("Вы классический зритель");
+    }else if (cnt > 30){
+        console.log("Вы киноман");
+    }else {
+        console.log("Произошла ошибка");
+    }
+}
+/* start();
+MyFilmRate();
+rememberMyFilms(); */
+showMyDB(personalMovieDB.privat);
+writeYourGenres(personalMovieDB.gennres);
 
-console.log(personalMovieDB);
